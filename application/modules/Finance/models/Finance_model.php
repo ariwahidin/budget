c2d1e45d0834b858efe6173738ab398bfd527679
ariwahidin <ariwahidin21@gmail.com>
2023-10-02 14:29:34 +0700

    ssss

--- application/modules/Finance/models/Finance_model.php
+++ application/modules/Finance/models/Finance_model.php
@@ -133,6 +133,9 @@ class Finance_model extends CI_Model
         }
 
         $sql .= "ORDER BY t1.CreatedDate DESC";
+
+        echo $sql;
+        exit;
         
         $query = $this->db->query($sql);
         return $query;
